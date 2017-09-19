67707002e51d9eb5362230ac89ab93165e7c919b
MJ Rossetti <datacreativellc@gmail.com>
2017-09-18 23:31:14 -0400

    Seed 2017 reports

--- app/jobs/report_seeder_job.rb
+++ app/jobs/report_seeder_job.rb
@@ -1,7 +1,19 @@
+require "csv"
+
 class ReportSeederJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    year = 2017
+    csv_file_path = Rails.root.join("db/seeds/batch_employment_reports/#{year}.csv")
+
+    batch = CSV.read(csv_file_path, headers: true)
+    batch.each do |row|
+      report = EmploymentReport.first_or_create({
+        school_name: row["SchoolName"],
+        year: year
+      })
+      report.update_attributes({total_grads: row["TotalGraduatesNumber"]})
+    end
   end
 end
