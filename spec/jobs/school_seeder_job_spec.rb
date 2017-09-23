3b2441962d22af765596e011646c66b352deb3f4
MJ Rossetti <datacreativellc@gmail.com>
2017-09-22 21:57:27 -0400

    WIP - Test link-parsing method

--- spec/jobs/school_seeder_job_spec.rb
+++ spec/jobs/school_seeder_job_spec.rb
@@ -15,8 +15,33 @@ RSpec.describe SchoolSeederJob, type: :job do
     #let(:school_links){ [{href: "https://my-school.edu", val: "MY SCHOOL 1"}]}
 
     it "should populate the schools table" do
-      expect(false).to eql(true)
-      #expect(School.count).to eql(school_links.count)
+      expect(School.count).to eql(school_links.count)
+    end
+  end
+
+  describe ".parse_link_content" do
+    let(:links){
+      [
+         {content: "AKRON (1961)", name: "AKRON", year:1961},
+         {content: "ARIZONA SUMMIT(formerly Phoenix - 2007)", name: "ARIZONA SUMMIT", year:2007},
+         {content: "CALIFORNIA - Berkeley (1923)", name: "CALIFORNIA - BERKELEY", year:1923},
+         {content: "CONCORDIA (2015)*", name: "CONCORDIA", year:2015},
+         {content: "LA VERNE (2006-2011; 2012)", name: "LA VERNE", year:2012},
+         {content: "MITCHELL|HAMLINE (2015)", name: "MITCHELL|HAMLINE", year:2015}, # consider 'MITCHELL - HAMLINE'
+         {content: "NEVADA (2000) ", name: "NEVADA", year:2000},
+         {content: "NEW ENGLAND LAW | BOSTON (1969)", name: "NEW ENGLAND LAW | BOSTON", year:1969}, # consider 'NEW ENGLAND LAW - BOSTON'
+         {content: "PENNSYLVANIA STATE-Penn State Law (1931)", name: "PENNSYLVANIA STATE-PENN STATE LAW", year:1931}, # consider "PENNSYLVANIA STATE - PENN STATE LAW"
+         {content: "ST. THOMAS (Florida) (1988)", name: "ST. THOMAS (FLORIDA)", year:1988},
+         {content: "UNT Dallas (2017)", name: "UNT DALLAS", year:2017}
+     ]
+    }
+
+    it "should parse the names and years of all the edge-cases" do
+      links.each do |link|
+        name, year = described_class.parse_link_content(link[:content])
+        expect(name).to eql(link[:name])
+        expect(year).to eql(link[:year])
+      end
     end
   end
 
