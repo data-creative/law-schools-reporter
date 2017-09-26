3150f1bf2e3a10dae3fb11bde211215ca9cffc81
MJ Rossetti <datacreativellc@gmail.com>
2017-09-26 17:34:38 -0400

    Heroku stop trying to use turbolinks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Tell Heroku to stop trying to use turbolinks.
+  config.serve_static_assets = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
