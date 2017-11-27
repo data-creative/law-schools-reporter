61ba0cb516aaed929c14a296d44ee51a802c3814
MJ Rossetti <datacreativellc@gmail.com>
2017-11-26 19:24:38 -0500

    Create a CSV file to store the annual rankings

--- app/jobs/usnews_rankings/part_time_rankings_scraper.rb
+++ app/jobs/usnews_rankings/part_time_rankings_scraper.rb
@@ -2,13 +2,15 @@
 #require "open-uri"
 #require "csv"
 
-class UsnewsRankings::ParttimeRankingsScraper < ApplicationJob
+class UsnewsRankings::PartTimeRankingsScraper < ApplicationJob
   queue_as :default
 
   FIRST_PAGE_URL = "https://www.usnews.com/best-graduate-schools/top-law-schools/part-time-law-rankings"
 
   def perform
     announce("SCRAPING PART-TIME RANKINGS FROM USNEWS.COM")
+
+    FileUtils.mkdir_p(results_csv_path)
   end
 
   def first_page_source
@@ -21,5 +23,9 @@ class UsnewsRankings::ParttimeRankingsScraper < ApplicationJob
 
   def rankings_year
     first_page_title.scan(/\d+/).first.to_i
+  end
+
+  def results_csv_path
+    Rails.root.join("db/seeds/usnews_rankings/part_time", "#{rankings_year}.csv")
   end
 end
--- spec/jobs/usnews_rankings/part_time_rankings_scraper_spec.rb
+++ spec/jobs/usnews_rankings/part_time_rankings_scraper_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UsnewsRankings::ParttimeRankingsScraper, type: :job do
+RSpec.describe UsnewsRankings::PartTimeRankingsScraper, type: :job do
   let(:job){ described_class.new }
 
   describe "#perform" do
@@ -8,10 +8,12 @@ RSpec.describe UsnewsRankings::ParttimeRankingsScraper, type: :job do
     let(:mock_first_page_source){ File.open(mock_first_page_path){|f| Nokogiri::HTML(f) } }
 
     let(:year){ 2017 }
-    let(:csv_file){ Rails.root.join("db/seeds/usnews_rankings/parttime", "#{year}.csv") }
+    let(:mock_results_csv_path){ Rails.root.join("spec/mocks/usnews_rankings/part_time", "#{year}.csv") }
 
     before(:each) do
+      FileUtils.rm_f(mock_results_csv_path) # clean-up before later testing existence of this file
       allow(job).to receive(:first_page_source).and_return(mock_first_page_source)
+      allow(job).to receive(:results_csv_path).and_return(mock_results_csv_path)
       job.perform
     end
 
@@ -25,11 +27,11 @@ RSpec.describe UsnewsRankings::ParttimeRankingsScraper, type: :job do
       #end
     end
 
-    #describe "writing" do
-    #  it "should store results in a single CSV file" do
-    #    expect(File.exist?(csv_file)).to eql(true)
-    #  end
-    #end
+    describe "writing" do
+      it "should store results in a single CSV file per year" do
+        expect(File.exist?(job.results_csv_path)).to eql(true)
+      end
+    end
   end
 
   describe "#rankings_year" do
